4d5258c7b5ba5caa3eaa9d08ee87bea6cf6bc656
Charon1223 <chlalstn9@gmail.com>
2024-03-28 13:51:39 +0900

    리덕스툴킷

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import Header from "./components/Header";
+import Auth from "./components/Auth";
+import UserProfile from "./components/UserProfile";
+import { useSelector } from "react-redux";
+
+function App() {
+  const isAuth = useSelector((state) => state.auth.isAuthenticated);
+
+  return (
+    <>
+      <Header />
+      {!isAuth && <Auth />}
+      {isAuth && <UserProfile />}
+    </>
+  );
+}
+
+export default App;
